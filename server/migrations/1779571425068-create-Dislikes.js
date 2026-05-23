554da0e0487589d203927ed2853c1d46e1db70f7
waqarahmed <waqartw1@gmail.com>
2026-05-24 02:24:00 +0500

    model changes

---
+++ server/migrations/1779571425068-create-Dislikes.js
@@ -0,0 +1,46 @@
+
+'use strict';
+
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('Dislikes', {
+      id: {
+        type: Sequelize.BIGINT,
+        autoIncrement: true,
+        primaryKey: true,
+        allowNull: false
+      },
+
+      user_id: {
+        type: Sequelize.BIGINT,
+        allowNull: false
+      },
+      target_user_id: {
+        type: Sequelize.BIGINT,
+        allowNull: false
+      },
+      is_mutual: {
+        type: Sequelize.BOOLEAN,
+        allowNull: true
+      },
+      is_seen: {
+        type: Sequelize.BOOLEAN,
+        allowNull: true
+      },
+
+      created_at: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+
+      updated_at: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('Dislikes');
+  }
+};
